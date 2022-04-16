49a344a4c22886232609e7f4e1c00da0942051f7
nicfv <nfmventura@gmail.com>
2022-04-16 13:43:03 -0700

    Added unit testing

--- Backend/Physical_Functions.py
+++ Backend/Physical_Functions.py
@@ -1,6 +1,6 @@
 
-CUTOFF_LO = 4 # [m/s]
-CUTOFF_HIGH = 5 # [m/s]
+CUTOFF_LO = 1 # [m/s]
+CUTOFF_HIGH = 25 # [m/s]
 CP_MAX = 0.593 # [unitless]
 
 # Convert from miles per hour to meters per second
@@ -12,17 +12,23 @@ def MPStoMPH(mps):
     return mps / 0.44704
 
 # Calculate the area of a circle (in meters^2)
-def CircleArea(diam):
-    return 3.1416/4 * diam**2
+def CircleArea(diameter):
+    return 3.1416/4 * diameter**2
 
 # Wind power (kinetic energy stored in the wind, in Watts)
-def WindPower(rho, A, V):
-    return 1/2 * rho * A * V**3
+def WindPower(density, area, velocity):
+    if velocity < CUTOFF_LO or velocity > CUTOFF_HIGH:
+        return 0
+    else:
+        return 1/2 * density * area * velocity**3
 
 # Turbine power output (in Watts)
-def TurbinePower(Pw, Cp):
-    return Pw * Cp
+def TurbinePower(windPower, coefficientOfPower):
+    if coefficientOfPower > CP_MAX:
+        raise Exception('Cp cannot be larger than ' + str(CP_MAX))
+    else:
+        return windPower * coefficientOfPower
 
 # Compute the air density adjustment based on the elevation (in meters) and the temperature (in Celsius)
-def AirDensity(elevationMeters, temperatureC):
-    return (1.225 - 1.194e-4 * elevationMeters) * (273.15 + 15) / (273.15 + temperatureC)
+def AirDensity(elevation, temperature):
+    return (1.225 - 1.194e-4 * elevation) * (273.15 + 15) / (273.15 + temperature)
