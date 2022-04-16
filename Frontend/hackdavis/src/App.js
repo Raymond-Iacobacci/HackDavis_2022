e702ee7a47990ae4876645ab29bf51ed4d9e3477
nicfv <nfmventura@gmail.com>
2022-04-16 16:52:24 -0700

    Added Nic bio

--- Frontend/hackdavis/src/App.js
+++ Frontend/hackdavis/src/App.js
@@ -11,10 +11,14 @@ function App() {
    * TODO: Type your bios here!
    */
   const Bios = {
-    Norman: '',
-    Jorge: '',
-    Nic: '',
-    Raymon: '',
+    Norman:
+      ``,
+    Jorge:
+      ``,
+    Nic:
+      `Hi! My name is Nicolas Ventura, or Nic. I am a master's student in Mechanical and Aerospace Engineering at UC Davis. I work at Lawrence Berkeley National Laboratory for building energy monitoring and efficiency, and I am passionate about saving energy and going green. Outside of work and academics, I love hiking, camping, and skiing, and I am a connoiseur of all things Star Wars.`,
+    Raymon:
+      ``,
   };
 
   return (
