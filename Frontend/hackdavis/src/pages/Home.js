8baab75a4b5e1efb9aa9055b9c0cdc2786106c7c
norman_mgu <norman.mgu@gmail.com>
2022-04-16 19:10:48 -0700

    changed title and favicon

--- Frontend/hackdavis/src/pages/Home.js
+++ Frontend/hackdavis/src/pages/Home.js
@@ -27,8 +27,8 @@ class Home extends React.Component {
                     <div class="container px-4 px-lg-5">
                         <div class="row gx-4 gx-lg-5 justify-content-center">
                             <div class="col-md-10 col-lg-8 col-xl-7">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
+                                <p>Welcome to the Renewable Energy Farm predictor. This collaboration comes from a group of UC Davis students with diverse background in order to help create renewable energy data easier to access. </p>
+                                <p>The goal of this project is to generate predictions of future wind speeds and their ideal directions. We want to predict changes in wind direction and wind speeds in the near future using machine learning. By predicting the future ideal conditions for wind turbines there will be an energy use reduction for turbines that are able to freely rotate as they use energy to rotate. This will allow them to decrease the amount of times they need to change their direction for wind. Another additional benefit is predicting models for potential wind turbines to be built in the near future. Figuring out how to predict these models will further the development of wind turbines throughout the US, in order to lower carbon emissions overall.</p>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
                             </div>
                         </div>
